1ee630921710630c6b71704553fdb78e42e1b27c
AbdullahBRashid <abdullahbinrashid2005abr@gmail.com>
2024-01-17 21:44:01 +0500

    Add basic dashboad

--- Front-End/src/Pages/Dashboard/Dashboard.tsx
+++ Front-End/src/Pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router-dom"
+import BaseDashboard from "./Bases/BaseDashboard"
+import MissileDashboard from "./Missiles/MissileDashboard"
+import MissilePage from "./Missiles/MissilePage"
+import { Button } from "../../Components/Button"
+
 function Dashboard() {
   return (
     <>
-        <h1>Dashboard</h1>
+      <h1 className="text-center">Dashboard</h1>
+
+      <div className="flex justify-center">
+        <Button className=" mr-5">
+          <a href="/dashboard/base">Bases</a>
+        </Button>
+        <Button>
+          <a href="/dashboard/missile">Missiles</a>
+        </Button>
+      </div>
+      <Routes>
+        <Route path="/base" element={<BaseDashboard />} />
+        <Route path="/missile" element={<MissileDashboard />} />
+        <Route path="/missile/:id" element={<MissilePage />} />
+      </Routes>
     </>
   )
 }
