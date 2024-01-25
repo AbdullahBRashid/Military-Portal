bbae7111da13742a096e98479b79641ae2c14d73
AbdullahBRashid <abdullahbinrashid2005abr@gmail.com>
2024-01-26 00:57:44 +0500

    Add basic footer

--- Front-End/src/Layout/Footer/Footer.tsx
+++ Front-End/src/Layout/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 
 function Footer() {
   return (
-    <footer>
-        <p>Footer</p>
+    <footer className="bg-black text-white p-4 text-center">
+      <p>&copy; 2024 Firebase Military Portal. All rights reserved.</p>
+      <p>Designed with ❤️ by <a className="text-red-400 hover:text-red-600 hover:underline" href="https://linkedin.com/in/AbdullahBRashid">Abdullah Bin Rashid</a></p>
     </footer>
   )
 }
